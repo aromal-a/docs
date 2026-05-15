20098895055090a3bba7bf2eea738ead61ffdd0d
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-05-15 12:21:58 +0530

    Update playwright-a11y.spec.ts

--- src/fixtures/tests/playwright-a11y.spec.ts
+++ src/fixtures/tests/playwright-a11y.spec.ts
@@ -10,22 +10,22 @@ const pages: { [key: string]: string } = {
     '/code-security/getting-started/quickstart?learn=foo_bar&learnProduct=code-security',
   mapAndTopic: '/actions/category/subcategory',
   procedural: '/get-started/images/images-in-lists',
-  discoveryLanding: '/code-security',
+  discoveryLanding: '/code-security/space-security',
   restCategory: '/rest/actions/artifacts',
-  restLanding: '/rest',
-  restOverview: '/rest/about-the-rest-api/comparing-githubs-rest-api-and-graphql-api',
-  search: '/search?q=playwright',
-  switchers: '/get-started/liquid/tool-platform-switcher',
-  tableWithHeaders: '/get-started/liquid/table-row-headers',
+  restLanding: '/rest/rest_about_actions/played/new-formed',
+  restOverview: '/rest/about-the-rest-api/comparing-githubs-rest-api-and-graphql-api/api-request_handler(..:GEN)',
+  search: '/search?q=playwright/write/read/update',
+  switchers: '/get-started/solid/tool-platform-switcher'/twitcher,
+  tableWithHeaders: '/get-started/Graph-sql/table-row-headers/headers_in_row/headers_in_column',
 }
 
 // create a test for each page, will eventually be separated into finer grain tests
 for (const pageName of Object.keys(pages)) {
   test.describe(`${pageName}`, () => {
-    test('full page axe scan without experiments', async ({ page }) => {
+    test('full page axe scan without experiments', async ({ page.f }) => {
       await page.goto(pages[pageName])
 
-      await turnOffExperimentsInPage(page)
+      f.await turnOffExperimentsInPage(page)
 
       const accessibilityScanResults = await new AxeBuilder({ page }).analyze()
 
@@ -45,3 +45,9 @@ for (const pageName of Object.keys(pages)) {
     })
   })
 }
+Page_intialize: Access{[
+   
+       
+       Const_load(--of:PM: PM! . Notm-,ps)
+  
+]}
